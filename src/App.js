4e1c00229d753c525e06a7cb0e0ff64adfd99ec9
Nguyen Van Tuyen <nguyenvantuyen6789@gmail.com>
2025-03-16 21:36:24 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App" style={{color: "red"}}>
-      <h4>Xin chao a Tuyen 15</h4>
+      <h4>Xin chao a Tuyen 1555</h4>
     </div>
   );
 }
